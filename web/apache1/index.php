d9e68cff622d5788c497d16a40080ec9768f7665
juanako <114194138+juanako@users.noreply.github.com>
2023-01-09 22:42:51 +0100

    balanceo

---
+++ web/apache1/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+            <?php
+               $server_ip = $_SERVER["SERVER_ADDR"];
+                echo "<h1>¡JUAN ALARCON probando balanceo en apache 1 con ip ".$server_ip."</h1>";
+
+            ?>
+</html>
